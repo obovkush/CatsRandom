556d7f66d60b0de25197b4137727fb9594885ff4
Oleg Bovkush <obovkush@gmail.com>
2022-08-26 13:53:32 +1000

    second commit

--- fetch.js
+++ fetch.js
@@ -2,13 +2,15 @@ const fetchButton = document.querySelector('#fetchButton');
 const fetchImg = document.querySelector('#fetchImg');
 console.log(fetchImg);
 
-fetchButton.addEventListener('click', async (e) => {
+const fetchCat = async (e) => {
     e.preventDefault();
     const response = await fetch('https://cataas.com/cat?json=true');
     const data = await response.json()
     fetchImg.src = `https://cataas.com/${data.url}`;
-});
+}
 
+fetchButton.addEventListener('click', (e) => fetchCat(e));
+fetchImg.addEventListener('click', (e) => fetchCat(e));
 const cursor = document.querySelector("#cursor");
     const mouseMove = function (e) {
     let x = e.clientX;
